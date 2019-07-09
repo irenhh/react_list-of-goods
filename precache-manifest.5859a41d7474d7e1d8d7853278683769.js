00c3d8c49b0d2ad659a23dc29f51ab897c584ed8
Iryna <iriskag@ukr.net>
2019-07-09 21:40:59 +0300

    Updates

---
+++ precache-manifest.5859a41d7474d7e1d8d7853278683769.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0412a8976179fefadcf839513f5b3f2a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_list-of-goods/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "10a93ddf91f099ae0c90",
+    "url": "/react_list-of-goods/static/js/main.fb9cf247.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.5859a41d7474d7e1d8d7853278683769.js"
 );
 
 self.addEventListener('message', (event) => {
